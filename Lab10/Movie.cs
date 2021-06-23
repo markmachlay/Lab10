e208124480ca5cc043ae45e800258bc0bd88013f
markmachlay <84604813+markmachlay@users.noreply.github.com>
2021-06-23 14:45:06 -0400

    Add files via upload

---
+++ Lab10/Movie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    public class Movie
+    {
+
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+        
+        public Movie()
+        {
+
+        }
+
+        public Movie(string category, string title)
+        {
+            Category = category;
+            Title = title;
+        }
+
+        public List<Movie> DisplayMovieList()
+        {
+            List<Movie> movieList = new List<Movie>();
+            movieList.Add(new Movie("Horror", "Saw"));
+            movieList.Add(new Movie("Horror", "Halloween"));
+            movieList.Add(new Movie("Horror", "Cabin in the Woods"));
+            movieList.Add(new Movie("Horror", "Final Destination"));
+            movieList.Add(new Movie("Animated", "The Iron Giant"));
+            movieList.Add(new Movie("Animated", "My Neighbor Totoro"));
+            movieList.Add(new Movie("Animated", "An American Tale"));
+            movieList.Add(new Movie("Animated", "The Lion King"));
+            movieList.Add(new Movie("Drama", "American Beauty"));
+            movieList.Add(new Movie("Drama", "Good Will Hunting"));
+            movieList.Add(new Movie("Drama", "Casablanca"));
+            movieList.Add(new Movie("Drama", "The Godfather"));
+            movieList.Add(new Movie("Sci-Fi", "The Matrix"));
+            movieList.Add(new Movie("Sci-Fi", "Alien"));
+            movieList.Add(new Movie("Sci-Fi", "Inception"));
+            movieList.Add(new Movie("Sci-Fi", "Star Wars: Empire Strikes Back"));
+
+            return movieList; 
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Movie List Application");
+            Console.WriteLine("There are many movies in the list");
+            char continueFlag;
+            do
+            {
+                Console.WriteLine("Please select a genre" + "\r\n" +
+               "1. Horror" + "\r\n" +
+               "2. Drama" + "\r\n" +
+               "3. Sci-Fi" + "\r\n" +
+               "4. Animated");
+                var userInput = Console.ReadLine();
+
+                var validate = IsUserValid(userInput);
+
+                var result = SelectAGenre(validate);
+
+                DisplayMoviesInGenre(result);
+                Console.WriteLine("Would you like to continue? (y/n)");
+                continueFlag = Console.ReadLine().ToLower()[0];
+            } while (continueFlag == 'y');
+           
+        }
+
+        static string IsUserValid(string userInput)
+        {
+            if(string.IsNullOrEmpty(userInput))
+            {
+                var response = string.Empty;
+                do
+                {
+                    Console.WriteLine("Please enter a number");
+                    response = Console.ReadLine();
+                } while (response == null);
+
+                return response;
+            }
+            else
+            {
+                return userInput;
+            }
+        }
+
+        static string SelectAGenre(string validate)
+        {
+            if(validate == "1" || validate == "Horror")
+            {
+                return "Horror";
+            }
+            else if(validate == "2" || validate == "Drama")
+            {
+                return "Drama";
+            }
+            else if(validate == "3" || validate == "Sci-Fi")
+            {
+                return "Sci-Fi";
+            }
+            else
+            {
+                return "Animated";
+            }
+        }
+
+        static void DisplayMoviesInGenre(string result)
+        {
+            Movie movies = new Movie();
+
+            var chosenMovies = movies.DisplayMovieList();
+
+            foreach (var movie in chosenMovies)
+            {
+                if (result == movie.Category)
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+        }
+    }
+}
